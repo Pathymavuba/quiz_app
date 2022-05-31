47fe52b1763c7876b418d4933aecadcc2f32190b
pathy <mavubapathy@gmail.com>
2022-05-31 05:44:36 +0100

    supression des commentaires inutiles

--- script.js
+++ script.js
@@ -28,109 +28,105 @@ const btn_quitter = document.querySelector("#btn-quitter")
 const son_check = new Audio("/checkson_sound.mp3")
 const son_resultat = new Audio("/score_sound.mp3")
 const sucess_resultat = new Audio("/success.mp3")
-
-
-// Les questions
 let questions = [{ 
-    // première question
     id: 0, q: "quel est le type d'un fichier javascript?", nq: "1/15",
-    rep: [{ text: ".ts", iscorect: false },   // iscorect est la valeur de la reponse
+    rep: [{ text: ".ts", iscorect: false },  
     { text: ".jsx", iscorect: false },
     { text: ".js", iscorect: true },
     { text: ".jv", iscorect: false }]
 },
-{   // deuxième question
+{   
     id: 1, q: "quel est le fondateur de javascript?", nq: "2/15",
     rep: [{ text: "Brendan Eich", iscorect: true },
     { text: "Steve Jobs", iscorect: false },
     { text: "Mark Elliot Zuckerberg", iscorect: false },
     { text: "Ronald Wayne", iscorect: false }]
 },
-{   // troisième question
+{   
     id: 2, q: "En quelle annéé  javascript est créé?", nq: "3/15",
     rep: [{ text: "1985", iscorect: false },
     { text: "1996", iscorect: false },
     { text: "1990", iscorect: false },
     { text: "1995", iscorect: true }]
 },
-{   // quatrième question
+{   
     id: 3, q: "Laquelle de ces syntaxes est correcte?", nq: "4/15",
     rep: [{ text: "if a <> 2 {}5", iscorect: false },
     { text: "if a != 2 {}", iscorect: false },
     { text: "if (a != 2) {}", iscorect: true },
     { text: "if (a <> 2) {}", iscorect: false }]
 },
-{    // cinqième question
+{    
     id: 4, q: " Quel attribut des noeuds de l'arbre DOM correspond à l'attribut HTML class ?", nq: "5/15",
     rep: [{ text: "class", iscorect: false },
     { text: "listClass", iscorect: false },
     { text: "className", iscorect: true },
     { text: "CLASS", iscorect: false }]
 },
-{   // sixième question
+{  
     id: 5, q: " Quelle méthode n'existe pas dans le DOM ?", nq: "6/15",
     rep: [{ text: "document.getElementsByAttribute", iscorect: true },
     { text: "document.getElementById", iscorect: false },
     { text: "document.getElementsByClassName", iscorect: false },
     { text: "document.getElementsByTagName", iscorect: false }]
 },
-{   // septième  question
+{  
     id: 6, q: "la méthode pour comparer deux chaînes texte est ?", nq: "7/15",
     rep: [{ text: "charCodeAt()", iscorect: false },
     { text: "localeCompare()", iscorect: true },
     { text: "charAt()", iscorect: false },
     { text: "indexOf()", iscorect: false }]
 },
-{  // huitième  question
+{  
     id: 7, q: "Quels sont les types de nombres définis en JavaScript ? ", nq: "8/15",
     rep: [{ text: "Number et Integer.", iscorect: false },
     { text: "Integer et Float.", iscorect: false },
     { text: "Number et Double.", iscorect: false },
     { text: "Number. ", iscorect: true }]
 },
-{   // neuvième  question
+{  
     id: 8, q: " Lequel de ces types d'événements n'existe pas ?", nq: "9/15",
     rep: [{ text: "blur ", iscorect: false },
     { text: " mouseclick ", iscorect: true },
     { text: " mouseout", iscorect: false },
     { text: "load ", iscorect: false }]
 },
-{   // dixième  question
+{  
     id: 9, q: "Quelle syntaxe est correcte pour que la fonction init soit appelée au chargement de la page ? ", nq: "10/15",
     rep: [{ text: "window.onload = init;", iscorect: true },
     { text: "window.onload = init();", iscorect: false },
     { text: "window.onload() = init;", iscorect: false },
     { text: "window.onload() = init();", iscorect: false }]
 },
-{   //onzième  question
+{   
     id: 10, q: "Peut-on accéder aux commentaires d'un document HTML ?", nq: "11/15",
     rep: [{ text: "Oui, avec Node.COMMENT_NODE ", iscorect: true },
     { text: "Oui, avec document.body.commentaries", iscorect: false },
     { text: "Non, ce n'est pas possible ", iscorect: false },
     { text: "Oui, avec node.nodeType évalué à 7;", iscorect: false }]
 },
-{   //douzième question
+{   
     id: 11, q: "for(; ; ) { ... } Que se passe-t-il avec cette instruction ?", nq: "12/15",
     rep: [{ text: " On obtient la valeur undefined", iscorect: false },
     { text: " On obtient la valeur null ", iscorect: false },
     { text: " Il ne se passe rien !", iscorect: false },
     { text: "Aucune bonne reponse", iscorect: true }]
 },
-{   // treizième question
+{  
     id: 12, q: "Lequel de ces codes n’affichera pas 3 ?", nq: "13/15",
     rep: [{ text: "alert(Math.max(-4, 3)", iscorect: false },
     { text: "aucune bonne reponse", iscorect: false },
     { text: "alert(Math.floor(2.9)", iscorect: true },
     { text: "var i = 3; alert(i++)", iscorect: false }]
 },
-{   // quatorzième  question
+{  
     id: 13, q: "Dans une boucle, l'instruction continue permet de :", nq: "14/15",
     rep: [{ text: " supprimer toutes les variables globales.", iscorect: false },
     { text: "continuer l'exécution du code après la boucle.", iscorect: false },
     { text: "revenir au début de l'itération courante.", iscorect: false },
     { text: "passer à l'itération suivante.", iscorect: true }]
 },
-{   // quinzième  question
+{   
     id: 14, q: " var a = 12; iNum %= 2; A la suite de cette expression, a vaut:", nq: "15/15",
     rep: [{ text: "6", iscorect: false },
     { text: "0.12", iscorect: false },
@@ -142,10 +138,7 @@ let label2 = document.getElementById("lb_2")
 let label3 = document.getElementById("lb_3")
 let label4 = document.getElementById("lb_4")
 let qst = document.getElementById("qst")
-let n_qst = document.getElementById("n_qst")
-
-
-// parcours de pages    
+let n_qst = document.getElementById("n_qst")   
 function questionnaire(id) {
     qst.textContent = questions[id].q;
     n_qst.textContent = questions[id].nq;
@@ -154,14 +147,12 @@ function questionnaire(id) {
     label3.textContent = questions[id].rep[2].text;
     label4.textContent = questions[id].rep[3].text;
 }
-//initialisation des radio
 function inititalisationradio(){
     all_radio[0].checked = false
     all_radio[1].checked = false
     all_radio[2].checked = false
     all_radio[3].checked = false
-}
-//recuperation des reponses   
+}  
 function radio_checked(id) {
     if (premier_choix.checked == true) {
         reponse_choisie = questions[id].rep[0].iscorect
@@ -178,7 +169,6 @@ function radio_checked(id) {
      inititalisationradio()
     return reponse_choisie
 }
-//recuperation de l'id
 function recuperation(){
     let monId;
     for (let i = 0 ; i < questions.length ; i++){
@@ -188,7 +178,6 @@ function recuperation(){
     }  
     return monId;  
 }
-// fonction progress and timer
 function gestion_progress() {
     if (mouv) {
         clearInterval(mouv)
@@ -236,10 +225,7 @@ function gestion_progress() {
     mouv = setInterval(ref, 1000)
 
 }
-
-//Affichage de la page-resultat
-function Affichage_resultat()
-{
+function Affichage_resultat(){
     form2.style.display = "none"
     form3.style.display = "block"
     name_result.textContent = nom.value
@@ -254,12 +240,10 @@ function Affichage_resultat()
         son_resultat.play()
     }
 }
-// Au clic du radio button
 for (let i = 0; i < 4; i++) {
     all_radio[i].addEventListener("change", () => {
         btn_suivant.disabled = false
         son_check.play()
-      // reinitialisation de la bordure verte des radio
         if (document.querySelector('.form-check.valid')) {
                document.querySelector('.form-check.valid').classList.remove('valid');
            }  
@@ -269,7 +253,6 @@ for (let i = 0; i < 4; i++) {
        })
    }
 btn_commencer.addEventListener("click", () => {
-    // gestion de validation coté client
     let position = email.value.indexOf("@gmail.com")
     let name = nom.value
     if (((nom.value == "")||(name.length <= 3))||((email.value == "")|| (position == -1)) ){
@@ -290,7 +273,6 @@ id = 1;
 reussite = 0;
 max = 15;
 btn_suivant.addEventListener("click", function (event) {
-    // reinitialisation de la bordure verte des radio
     if (document.querySelector('.form-check.valid')) {
         document.querySelector('.form-check.valid').classList.remove('valid');
     }
@@ -317,13 +299,9 @@ btn_suivant.addEventListener("click", function (event) {
     }
 
 })
-
-// quitter et avoir le score
 btn_quitter.addEventListener("click", () => {
     Affichage_resultat()
 })
-
-//revenir à la prémière page pour rejouer
 const btn_accueil = document.getElementById("btn-accueil")
 btn_accueil.addEventListener("click", () => {
     form3.style.display = "none"
